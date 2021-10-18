6b3fa803795c817a05e27cde869dbcf3abb7173b
sideov <90581598+sideov@users.noreply.github.com>
2021-10-18 19:21:32 +0300

    1

---
+++ HW3/functions.h
@@ -0,0 +1,21 @@
+
+#ifndef UNTITLED_FUNCTIONS_H
+#define UNTITLED_FUNCTIONS_H
+
+struct Point {
+    double x;
+    double y;
+};
+
+struct Vector2D {
+    double x;
+    double y;
+};
+
+struct Barrier {
+    double x;
+    double h;
+};
+
+
+#endif //UNTITLED_FUNCTIONS_H
--- cmake-build-debug/HW3/cmake_install.cmake
+++ cmake-build-debug/HW3/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/maximvasilev/CLionProjects/PR
+# Install script for directory: /Users/maximvasilev/CLionProjects/PR/HW3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,28 +37,3 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/Users/maximvasilev/CLionProjects/PR/cmake-build-debug/HW1/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/Users/maximvasilev/CLionProjects/PR/cmake-build-debug/HW2/cmake_install.cmake")
-endif()
-
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("/Users/maximvasilev/CLionProjects/PR/cmake-build-debug/HW3/cmake_install.cmake")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/maximvasilev/CLionProjects/PR/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
