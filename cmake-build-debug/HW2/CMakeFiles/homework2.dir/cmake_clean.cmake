428768d4e21a9dcb48bbd1379031f0613e330566
sideov <90581598+sideov@users.noreply.github.com>
2021-10-25 14:55:58 +0300

    fixed

---
+++ cmake-build-debug/HW2/CMakeFiles/homework2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework2.dir/functions.cpp.o"
+  "CMakeFiles/homework2.dir/main.cpp.o"
+  "homework2"
+  "homework2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
