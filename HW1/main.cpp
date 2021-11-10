865c475af5e9b17343db9e5ce32ef66fbc074c87
sideov <90581598+sideov@users.noreply.github.com>
2021-11-10 15:01:46 +0300

    fixed

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -48,7 +48,7 @@ int main() {
 
             //cout << Vectors[i].x << " " << Vectors[i].y << " h = " << distance(MVector, Vectors[i]) << endl;
             double coss = cosangle(MVector, Vectors[i]);
-            coss = round(coss * 10000000000.0) / 10000000000.0;
+
             //cout << anglee << " ";
             if (isright(MVector, Vectors[i])) {
                 //cout << "Right" << endl;
