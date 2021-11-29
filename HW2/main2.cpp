be4e97ffce4b87d2aa4490eb598fb6cc02e14c2e
sideov <90581598+sideov@users.noreply.github.com>
2021-11-29 19:02:03 +0300

    fixed

--- HW2/main2.cpp
+++ HW2/main2.cpp
@@ -12,68 +12,65 @@ using namespace std;
 int main(int argc, char** argv) {
     string file_name = "";
 
-    if (argc == 2){
+    if (argc == 2) {
         file_name = string(argv[1]);
-    } else{
+    }
+    vector <Barrier> Barriers;
+    double h;
+    Vector2D V{};
+    Barrier B;
 
-        vector <Barrier> Barriers;
-        double h;
-        Vector2D V{};
-        Barrier B;
+    ifstream file(file_name);
+    file >> h;
+    file >> V.x >> V.y;
 
-        ifstream file(file_name);
-        file >> h;
-        file >> V.x >> V.y;
+    while (file >> B.x >> B.h) {
+        Barriers.push_back(B);
+    }
+    int l = Barriers.size();
+    file.close();
 
-        while (file >> B.x >> B.h) {
-            Barriers.push_back(B);
+
+    const double g = 9.81;
+    Point Point{};
+    Point.x = 0;
+    Point.y = h;
+    int cur_bar = -1;
+    int direction = 1;
+
+
+    while (true) {
+
+        int m = cur_bar + direction;
+        if (m<0) {
+            cout << 0;
+            return 0;
         }
-        int l = Barriers.size();
-        file.close();
+        double x = Barriers[m].x;
+        double bar = Barriers[m].h;
+        double t = abs((x - Point.x)) / abs(V.x);
+        double fall_time = (V.y + sqrt(V.y*V.y+2*g*Point.y))/g;
 
 
-        const double g = 9.81;
-        Point Point{};
-        Point.x = 0;
-        Point.y = h;
-        int cur_bar = -1;
-        int direction = 1;
+        if (m > l-1) {
+            cout << l;
+            return 0;
+        }else if (fall_time < t) {
+            if (direction == 1) cout << m;
+            else cout << m + 1;
+            return 0;
 
+        }
+        double y = Point.y + V.y * t - (g * t * t) / 2;
 
-        while (true) {
+        V.y = V.y-g*t;
+        Point.x = x;
+        Point.y = y;
+        cur_bar = m;
 
-            int m = cur_bar + direction;
-            if (m<0) {
-                cout << 0;
-                return 0;
-            }
-            double x = Barriers[m].x;
-            double bar = Barriers[m].h;
-            double t = abs((x - Point.x)) / abs(V.x);
-            double fall_time = (V.y + sqrt(V.y*V.y+2*g*Point.y))/g;
-
-
-            if (m > l-1) {
-                cout << l;
-                return 0;
-            }else if (fall_time < t) {
-                if (direction == 1) cout << m;
-                else cout << m + 1;
-                return 0;
-
-            }
-            double y = Point.y + V.y * t - (g * t * t) / 2;
-
-            V.y = V.y-g*t;
-            Point.x = x;
-            Point.y = y;
-            cur_bar = m;
-
-            if (y < bar) {
-                V.x = -V.x;
-                direction = V.x/abs(V.x);
-            }
+        if (y < bar) {
+            V.x = -V.x;
+            direction = V.x/abs(V.x);
         }
     }
-
 }
