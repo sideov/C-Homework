938bbee8bc0d14bdd02c8c777ba3d5d7955259fb
sideov <90581598+sideov@users.noreply.github.com>
2021-11-29 14:33:39 +0300

    fixed

---
+++ HW3/main3.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+class Polygon()
+{
+public:
+    int n;
+    new vector <int> points;
+
+    void add_point(float x, float y){
+        points.push_back({x, y})
+    }
+
+}
